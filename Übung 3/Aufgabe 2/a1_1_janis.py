41485cace016b4706ac79da24a15f3945a1396dc
ItamiOfficial <57113216+ItamiOfficial@users.noreply.github.com>
2019-11-13 19:05:39 +0100

    Add files via upload

---
+++ Übung 3/Aufgabe 2/a1_1_janis.py
@@ -0,0 +1,44 @@
+def find_pos (zeichen,buchstabe): 
+    l=0
+    if zeichen == "":
+        return(-1)
+    if zeichen != "":
+        for char in zeichen:
+            if char == buchstabe:
+                print(zeichen)
+                return(l)
+            l=l+1
+    for char in zeichen:
+        if char != buchstabe:
+            return(-1)
+    
+zeichen=input(str("Zeichenkette : " ))
+buchstabe=input("Buchstabe : " )
+out1= find_pos(zeichen,buchstabe)
+print(out1)
+#Teilaufgabe a
+
+f=open("de_DE_frami.txt")
+n = open("dictionary.txt","w")
+line = f.readline()
+i = 0
+while line != "":
+    #print (repr(line))
+    print(i)
+    i = i+1
+    if "#" not in line:
+        if "\t\n" not in line:
+            for slash in line:
+                if slash == "/":
+                    slashindex = line.index(slash)
+                    line = line[0:slashindex]
+                    line = line + "\n"
+         n.write(line)
+    
+    line = f.readline()
+    if i == 100:
+        break
+f.close()
+n.close()
+print("bin feddisch alda")
+
